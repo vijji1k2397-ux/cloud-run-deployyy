7c252643b85f30030fab0dfc223ea8e24d816173
vijji1k2397-ux <vijji1k2397@gmail.com>
2025-09-04 10:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
     return "cloud run deploy using github actions!"
 
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+
+
